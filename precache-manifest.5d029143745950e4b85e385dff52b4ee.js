c146f13d7402caf5f1010735bcd56fc4acbc1338
alex05d <alexiss.diaz@live.com>
2021-06-04 19:06:12 -0500

    Updates

---
+++ precache-manifest.5d029143745950e4b85e385dff52b4ee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "56c2abadb92810b102a5",
+    "url": "/resume/static/css/main.8164ba2c.chunk.css"
+  },
+  {
+    "revision": "56c2abadb92810b102a5",
+    "url": "/resume/static/js/main.56c2abad.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/resume/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "18bea22a2a5d1e1d102c",
+    "url": "/resume/static/js/runtime~main.18bea22a.js"
+  },
+  {
+    "revision": "414d8ba4e0b3e3e5dc932aeb092e737a",
+    "url": "/resume/index.html"
+  }
+];
